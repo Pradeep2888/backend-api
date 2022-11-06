7e42c9a7034230983ec95cfa34cf3334b215e258
Pradeep2888 <pradeeptiwari2888@gmail.com>
2022-11-06 23:44:36 +0530

    pradeep

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT=process.env.PORT || 8000
 app.get("/",(req,res)=>{
     res.send("welcome to home")
 })
+app.get("/about",(req,res)=>{
+    res.send("welcome to about")
+})
 
 app.listen(PORT,()=>{
     console.log(`listen to ${PORT}`)
